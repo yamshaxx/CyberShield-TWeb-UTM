ba328299c87fb98d05f07363433a406790b4cff8
yamshaxx <yamshaxx17@gmail.com>
2025-03-22 16:08:27 +0200

    Added a part of the auth

---
+++ CyberShield.BusinessLogic/BL Struct/AuthBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyberShield.BusinessLogic.Core;
+using CyberShield.BusinessLogic.Interface;
+using CyberShield.Domain.Model.User;
+
+namespace CyberShield.BusinessLogic.BL_Struct
+{
+    public class AuthBL : UserApi, IAuth
+    {
+        public string UserAuthLogic(UserLoginDTO data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CyberShield.BusinessLogic/Interface/IAuth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyberShield.Domain.Model.User;
+
+namespace CyberShield.BusinessLogic.Interface
+{
+    public interface IAuth
+    {
+        string UserAuthLogic(UserLoginDTO data);
+    }
+}
