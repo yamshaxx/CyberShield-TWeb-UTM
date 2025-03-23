00e10560e2d20fc3d3edc87aa77b2e1f55d61984
yamshaxx <yamshaxx17@gmail.com>
2025-03-23 14:42:09 +0200

    Added BL Network Pentest

--- CyberShield.BusinessLogic/BusinessLogic.cs
+++ CyberShield.BusinessLogic/BusinessLogic.cs
@@ -4,22 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CyberShield.BusinessLogic.BL_Struct;
+using CyberShield.BusinessLogic.BL_Struct.PenetrationTesting;
 using CyberShield.BusinessLogic.Interface;
+using CyberShield.BusinessLogic.Interface.Repositories;
 
 namespace CyberShield.BusinessLogic
 {
     public class BusinessLogic
     {
         private readonly IAuth _authBL;
+        private readonly INetworkPentestService _networkPentestService;
 
-        public BusinessLogic()
+        public BusinessLogic(INetworkPentestRepository networkPentestRepository, IClientRepository clientRepository)
         {
             _authBL = new AuthBL();
+            _networkPentestService = new NetworkPentestService(clientRepository, networkPentestRepository);
         }
 
         public IAuth GetAuthBL()
         {
             return _authBL;
+        }
+
+        public INetworkPentestService GetNetworkPentestService()
+        {
+            return _networkPentestService;
         }
     }
 }
---
+++ CyberShield.BusinessLogic/Interface/Repositories/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyberShield.Domain.Model.Client;
+
+namespace CyberShield.BusinessLogic.Interface.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<ClientDTO> GetByIdAsync(int id);
+        Task<ClientDTO> GetByEmailAsync(string email);
+        Task<int> CreateAsync(ClientDTO client);
+        Task UpdateAsync(ClientDTO client);
+    }
+}
