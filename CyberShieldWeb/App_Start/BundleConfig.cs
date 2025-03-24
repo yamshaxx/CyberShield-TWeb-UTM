69c5b52d71cbbbab9beb8e24b6a799f46b26f742
yamshaxx <yamshaxx17@gmail.com>
2025-03-24 09:11:29 +0200

    Added more stuff to Services, Added an Index there and Pentest page, a footer and images

--- CyberShieldWeb/App_Start/BundleConfig.cs
+++ CyberShieldWeb/App_Start/BundleConfig.cs
@@ -10,7 +10,6 @@ namespace CyberShieldWeb.App_Start
 	{
         public static void RegisterBundle(BundleCollection bundles)
         {
-
             bundles.Clear();
 
             // CSS Bundles
@@ -24,13 +23,18 @@ namespace CyberShieldWeb.App_Start
                 "~/Content/css/index.css");
             bundles.Add(cssBundle);
 
+            // Favicon Bundle
+            var faviconBundle = new StyleBundle("~/Content/favicon");
+            faviconBundle.Include("~/favicon.ico");
+            bundles.Add(faviconBundle);
+
             // Site Scripts Bundle
             var siteBundle = new ScriptBundle("~/bundles/site");
             siteBundle.Include(
+                "~/Scripts/site.js",
                 "~/Scripts/js/site.js",
                 "~/Scripts/js/main.js");
             bundles.Add(siteBundle);
-
 
             BundleTable.EnableOptimizations = false;
         }
