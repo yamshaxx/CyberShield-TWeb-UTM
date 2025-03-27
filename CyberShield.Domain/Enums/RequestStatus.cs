8f9f5ae833bedf3f8abdc1d83992dc05dedd4d41
yamshaxx <yamshaxx17@gmail.com>
2025-03-27 15:18:10 +0200

    Added BL Network Pentest

---
+++ CyberShield.Domain/Enums/RequestStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberShield.Domain.Enums
+{
+    public enum RequestStatus
+    {
+        New,
+        InProgress,
+        Completed,
+        Canceled
+    }
+}
